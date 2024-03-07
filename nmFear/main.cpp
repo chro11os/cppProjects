50db795ceea51336d4b037753278832f5dc3d2de
chrollos web dev <106138109+chro11os@users.noreply.github.com>
2024-03-07 09:39:18 +0800

    Migrated the main File to the updated cpp directory
    Shims

---
+++ nmFear/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+class Login 
+{
+	public:	
+	struct
+	{
+		char choice;
+		string pass;
+		string user;
+	} login;
+	
+	void Log()
+	{
+		cout << "Enter a Username: ";
+		cin >> login.user;
+			
+		cout << "Enter a Password: ";
+		cin >> login.pass;
+	}
+	
+};
+
+class SignIn
+{
+	public:
+	struct
+	{
+		string signpass;
+		string signuser;
+	} sign;
+	
+	void Sign()
+	{
+		cout << "Enter a desired Username: ";
+		cin >> sign.signuser;
+			
+		cout << "Enter a desired Password: ";
+		cin >> sign.signpass;
+	}
+					
+};
+		
+int main()
+{
+	
+	cout << "1. Login" << endl;
+	cout << "2. Sign in" << endl;
+	cout << "3. Trade" << endl;
+	cout << "4. Buy" << endl;
+	cout << "5. Sell" << endl;
+	cout << "6. Exit" << endl;
+	cout << " " << endl;
+	char choice;
+
+	cout << "Enter Choice: ";
+	cin >> choice;
+
+	switch (choice)
+	{
+		case '1':
+			Login log;
+			log.Log();
+			return 0;
+		break;
+		
+		case '2':
+			SignIn sign;
+			sign.Sign();
+			return 0;
+		break;
+	}
+		
+}
+
+	

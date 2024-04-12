25e1b3bba4d3c58dc7f89f09a0ac1f112f2dad1c
chrollos web dev <106138109+chro11os@users.noreply.github.com>
2024-04-12 14:01:26 +0800

    fixed bracket
    small fix only

--- AdvancedProjects/MainTextBasedGame/MainTextBasedGame.cpp
+++ AdvancedProjects/MainTextBasedGame/MainTextBasedGame.cpp
@@ -4,15 +4,15 @@
 #include "MainStruct.h"
 using namespace std;
 
-int main()
-{
+int main() {
+
     setting set;
     object obj;
     animal anim;
     weapons weap;
     defense def;
     enemy enem;
-    rule rl;
+    rule rl; 
     
     cout << ""
 

5d5e49fcaea82f1898eef36c12737de2fe8e57a2
chrollos <106138109+chro11os@users.noreply.github.com>
2024-10-11 19:12:45 +0800

    started work on the lexical analyzer

---
+++ cppCompiler/compiler/lexicalAnalyzer/Token.h
@@ -0,0 +1,23 @@
+//
+// Created by chro11os on 10/11/24.
+//
+
+#ifndef TOKEN_H
+#define TOKEN_H
+#include <string>
+
+
+enum TokenType {
+    KEYWORD, IDENTIFIER, NUMBER, OPERATOR, PUNCTAION, END_OF_FILE
+};
+
+
+struct Token {
+    enum TokenType type;
+    std::string value;
+
+    Token(TokenType type, std::string value) : type(type), value(value) {}
+};
+
+
+#endif //TOKEN_H
